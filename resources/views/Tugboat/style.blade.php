655b48989c8291a0316f7381e949796e589b11f5
reign <r_em11.malto@yahoo.com>
2018-07-23 01:38:28 +0800

    Reign
    Reign

---
+++ resources/views/Tugboat/style.blade.php
@@ -0,0 +1,8 @@
+        <link rel="stylesheet" href="/others/stisla_admin_v1.0.0/dist/css/smart_wizard.min.css">
+        <link rel="stylesheet" href="/others/stisla_admin_v1.0.0/dist/css/smart_wizard_theme_arrows.min.css">
+        <link rel="stylesheet" href="/others/stisla_admin_v1.0.0/dist/css/smart_wizard_theme_circles.min.css">
+
+        <link rel="stylesheet" href="/others/stisla_admin_v1.0.0/dist/css/animate.min.css">
+        <link rel="stylesheet" href="/others/stisla_admin_v1.0.0/dist/css/custom.css">
+        <link rel="stylesheet" href="/others/stisla_admin_v1.0.0/dist/css/cssAll.css">
+        <link rel="stylesheet" href="/others/stisla_admin_v1.0.0/dist/css/noCard.css">
